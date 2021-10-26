4ca6debc5454b685dc96784184d58e2b6cf4d445
Sushant Sharma <sushant.x01@gmail.com>
2021-10-26 21:40:21 +0530

    Basic Translator

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let mainData = document.getElementById("data");
+
+async function getData(){
+
+ let txtArea = document.getElementById('txtarea').value;
+ let lang = document.getElementById('langs').value;
+ 
+const res = await fetch("https://libretranslate.de/translate", {
+	method: "POST",
+	body: JSON.stringify({
+		q: `${txtArea}`,
+		source: "en",
+		target: `${lang}`
+	}),
+	headers: { "Content-Type": "application/json" }
+});
+let data = await res.json();
+appendData(data);
+}
+
+function appendData(el){
+   
+    mainData.innerHTML = null;
+
+   let p = document.createElement("p");
+   p.innerHTML = el.translatedText;
+
+   mainData.append(p);
+}
